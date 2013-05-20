167e13e6d89b8a9adddfaab81302e34b48e182c3
jrpavoncello <jrpavoncello@gmail.com>
2013-05-20 13:27:33 -0400

    Added with last commit.

---
+++ OrbitMapper/Shapes/Tri3060.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OrbitMapper
+{
+    public class Tri3060 : Shape
+    {
+        public Tri3060()
+        {
+            base.Text = "Tri3060";
+            base.Name = "Tri3060" + base.getTabCount();
+            base.setTabCount(base.getTabCount() + 1);
+            double leftWallLength = 1024d * Math.Cos(Math.PI / 3);
+            double height = Math.Sin(Math.PI / 3) * leftWallLength;
+            double smallBottomWallLength = leftWallLength * Math.Sin(Math.PI / 3);
+            addVertex(0, 0, 0);
+            addVertex(smallBottomWallLength, height, 60d);
+            addVertex(1024d, 0, 150d);
+            setScale(1024d, height);
+            setStartArea(0d, 1024);
+        }
+    }
+}
---
+++ OrbitMapper/Tessellations/Tri3060Tess.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrbitMapper
+{
+    public partial class Tri3060Tess : Tessellation
+    {
+        public Tri3060Tess()
+        {
+            InitializeComponent();
+            double heightOnSide = Math.Sin(Math.PI / 3d) * 30d;
+            double segmentHeight = Math.Tan(Math.PI / 6d) * 30d;
+            double triH = Math.Sin(Math.PI / 6d) * 30d;
+            double height = Math.Sqrt(900 + (segmentHeight * segmentHeight));
+            double totalHeight = height + segmentHeight;
+            Pattern pat = new Pattern(60, totalHeight, 30);
+            List<doublePoint> tri1 = new List<doublePoint>();
+            List<doublePoint> tri2 = new List<doublePoint>();
+            List<doublePoint> tri3 = new List<doublePoint>();
+            addStartZone(new Point(30, 0), new Point(60, 0));
+            addReflectedStartZone(new Point(0, 0), new Point(30, 0));
+            setShapeHeight(heightOnSide);
+
+            tri1.Add(new doublePoint(30 - (Math.Cos(Math.PI / 3d) * 30d), heightOnSide));
+            tri1.Add(new doublePoint(0, segmentHeight));
+            tri1.Add(new doublePoint(0, 0));
+            tri1.Add(new doublePoint(30, 0));
+            tri1.Add(new doublePoint(30 - (Math.Cos(Math.PI / 3d) * 30d), heightOnSide));
+            tri1.Add(new doublePoint(30, height));
+            tri1.Add(new doublePoint(30 + triH, heightOnSide));
+            tri1.Add(new doublePoint(30, 0));
+            tri1.Add(new doublePoint(60, 0));
+            tri1.Add(new doublePoint(60, segmentHeight));
+            tri1.Add(new doublePoint(30 + triH, heightOnSide));
+            tri1.Add(new doublePoint(30, 0));
+            tri1.Add(new doublePoint(60, segmentHeight));
+            tri1.Add(new doublePoint(30, 0));
+
+            tri2.Add(new doublePoint(60 - (Math.Cos(Math.PI / 3d) * 30d), totalHeight - heightOnSide));
+            tri2.Add(new doublePoint(30, totalHeight - segmentHeight));
+            tri2.Add(new doublePoint(30, totalHeight));
+            tri2.Add(new doublePoint(60, totalHeight));
+            tri2.Add(new doublePoint(60 - (Math.Cos(Math.PI / 3d) * 30d), totalHeight - heightOnSide));
+            tri2.Add(new doublePoint(60, totalHeight - height));
+            tri2.Add(new doublePoint(60 + triH, totalHeight - heightOnSide));
+            tri2.Add(new doublePoint(60, totalHeight));
+            tri2.Add(new doublePoint(90, totalHeight));
+            tri2.Add(new doublePoint(90, totalHeight - segmentHeight));
+            tri2.Add(new doublePoint(60 + triH, totalHeight - heightOnSide));
+            tri2.Add(new doublePoint(60, totalHeight));
+
+            tri3.Add(new doublePoint(0, segmentHeight));
+            tri3.Add(new doublePoint(30, 0));
+            tri3.Add(new doublePoint(30, height));
+            tri3.Add(new doublePoint(30, 0));
+
+
+            pat.addPattern(tri1);
+            pat.addPattern(tri2);
+            pat.addPattern(tri3);
+            setPattern(pat);
+        }
+    }
+}
